9301698e4725a2efa83db35942738c10d85686e4
sakinabehbudova <sakina.behbudova1996@gmail.com>
2020-05-11 23:59:46 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+  $('.item').mouseover(function () {
+  $(this).css("color","#DAA520");
+  $(this).css("transition-duration","0.5s");
+})
+
+    $('.item').mouseout(function () {
+    $(this).css("color","white");
+    $(this).css("transition-duration","0.5s");
+  })
+
+  $('.btn').mouseover(function () {
+  $(this).css("color","lightgrey");
+  $(this).css("transition-duration","0.5s");
+})
+
+$('.btn').mouseout(function () {
+  $(this).css("color","black");
+  $(this).css("transition-duration","0.5s");
+})
