8db99cf308e0323b196fd519364798a70022bbd7
sakinabehbudova <sakina.behbudova1996@gmail.com>
2020-05-22 23:47:51 +0400

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+//smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 
   $('.item').mouseover(function () {
   $(this).css("color","#DAA520");
